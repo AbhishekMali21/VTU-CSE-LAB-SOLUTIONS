6fa530e26443ca4947ff9a0bfb7e2b600635f13c
Abhishek Mali <43645320+AbhishekMali21@users.noreply.github.com>
2019-02-23 23:55:35 +0530

    LEX - ELIMINATE COMMENT LINES IN A C PROGRAM

---
+++ 6th SEM/SYSTEM SOFTWARE AND OPERATING SYSTEM LABORATORY/f1.c
@@ -0,0 +1,8 @@
+#include<stido.h>
+
+int main()
+{
+	// this is a comment 
+	printf("hello");
+	/* this is another comment */
+}
---
+++ 6th SEM/SYSTEM SOFTWARE AND OPERATING SYSTEM LABORATORY/f2.c
@@ -0,0 +1,6 @@
+#include<stido.h>
+
+int main()
+{
+	printf("hello");
+}
