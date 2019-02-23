329b4e84329822de41fb3f0be9eae1d27221ac39
Abhishek Mali <43645320+AbhishekMali21@users.noreply.github.com>
2019-02-24 00:15:58 +0530

    C - A NUMERICAL CALCULATOR

---
+++ 6th SEM/SYSTEM SOFTWARE AND OPERATING SYSTEM LABORATORY/LAB10a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+	char operator;
+	float num1, num2, result;
+	printf("Simulation of a Simple Calculator\n\n");
+	printf("Enter two numbers \n");
+	scanf("%f %f", &num1,&num2);
+
+	fflush(stdin);
+	printf("\nEnter the operator [+,-,*,/] \n");
+	scanf("%s", &operator);
+
+	switch(operator)
+	{
+		case '+': result = num1 + num2;		break;
+		case '-': result = num1 - num2;		break;
+		case '*': result = num1 * num2;		break;
+		case '/': result = num1 / num2;		break;
+		default : printf("Error in operation");     break;
+	}
+	printf("\n%.2f %c %.2f = %.2f\n", num1, operator, num2, result);
+}
