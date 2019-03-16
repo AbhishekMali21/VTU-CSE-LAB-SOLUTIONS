253fe0078823c1cb4c5a97232db828538cd68aab
Abhishek Mali <43645320+AbhishekMali21@users.noreply.github.com>
2019-03-16 15:45:10 +0530

    PHP - DIGITAL CLOCK

---
+++ 7th SEM/WEB TECHNOLOGY LABORATORY/PROG7.php
@@ -0,0 +1,17 @@
+<head>
+	<meta http-equiv="refresh" content="1"/>
+	<style>
+		p {
+			color:yellow;
+			font-size:90px;
+			position:absolute;
+			top: 40%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+		body {
+			background-color:maroon;
+		}
+	</style>
+	<p> <?php echo date(" h: i : s A");?> </p>
+</head>
