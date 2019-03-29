b97450a4631ae3bd39e0d82393c9ca30c36ac375
Abhishek Mali <43645320+AbhishekMali21@users.noreply.github.com>
2019-03-29 23:16:42 +0530

    CODE FILE

--- 2nd SEM/COMPUTER PROGRAMMING LABORATORY/LAB2.c
+++ 2nd SEM/COMPUTER PROGRAMMING LABORATORY/LAB2.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
-#include<conio.h>
+
 void main()
 {
     int a,b;
     int op;
-    clrscr();
 
     printf(" 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n 5.Remainder\n");
     printf("Enter the values of a & b: ");
-    scanf("%d %d",&a,&b);
-    
+    scanf("%d %d",&a,&b);  
     printf("Enter your Choice : ");
     scanf("%d",&op);
     
@@ -34,5 +32,4 @@ void main()
     	   printf(" Enter Your Correct Choice.");
     	   break;
     }
-    getch();
 }
