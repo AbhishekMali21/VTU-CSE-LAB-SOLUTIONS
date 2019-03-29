e93fbcba2e2b86a80c80cfcd51d980a8b7823693
Abhishek Mali <43645320+AbhishekMali21@users.noreply.github.com>
2019-03-29 23:30:33 +0530

    CODE FILE

--- 2nd SEM/COMPUTER PROGRAMMING LABORATORY/LAB4.c
+++ 2nd SEM/COMPUTER PROGRAMMING LABORATORY/LAB4.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-#include<conio.h>
 
-main()
+int main()
 {
    int n,m,rev,digit;
-   clrscr();
-   printf("enter the value of n\n");
+   printf("Enter the value of n\n");
    scanf("%d",&n);
 
    rev=0;
@@ -16,11 +14,10 @@ main()
       n=n/10;
       rev=digit+10*rev;
    }
-   printf("reverse of %d is %d\n",m,rev);
+   printf("Reverse of %d is %d\n",m,rev);
 
    if(m==rev)
 	     printf("It is a palindrome\n");
    else
 	     printf("It is not a palindrome\n");
-   getch();
 }
