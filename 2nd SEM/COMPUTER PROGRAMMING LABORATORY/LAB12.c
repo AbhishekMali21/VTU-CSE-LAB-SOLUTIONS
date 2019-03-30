690e7152686e98716343f57525623743f355be8c
Abhishek Mali <43645320+AbhishekMali21@users.noreply.github.com>
2019-03-30 04:00:10 -0400

    CODE FILE

--- 2nd SEM/COMPUTER PROGRAMMING LABORATORY/LAB12.c
+++ 2nd SEM/COMPUTER PROGRAMMING LABORATORY/LAB12.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
 
-main()
+int main()
 {
   float n,result;
-  clrscr();
   printf("enter the value of n\n");
   scanf("%f",&n);
-
   result=pow(n,0.5);
   printf("square root of %f is %f\n",n,result);
-  getch();
 }
